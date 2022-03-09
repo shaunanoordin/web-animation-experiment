372ed8aae8cf1ece0bcfc4ade7b3e4f818c77e6c
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2022-03-09 15:50:10 +0000

    Add Section 2 with basic programmatic animation

--- src/main.js
+++ src/main.js
@@ -1,25 +1,34 @@
-/*  
-Starter JS
-----------
-
-Starter template for JavaScript web app projects.
-
-(Shaun A. Noordin | shaunanoordin.com | 20180413)
- */
-
-import {ImportExample} from "./importExample.js";
-
 /*  Primary App Class
  */
 //==============================================================================
 class App {
   constructor() {
-    let importExample = new ImportExample("HI THERE");
-    
-    this.console = document.getElementById("console");
-    this.console.innerHTML =
-      "This is a starter template for JS projects. <br>" +
-      importExample.getText();
+    this.html = {
+      s2Object: document.getElementById('s2-object'),
+      s2ColourA: document.getElementById('s2-colour-a'),
+      s2ColourB: document.getElementById('s2-colour-b'),
+      s2StartButton: document.getElementById('s2-start-button'),
+    }
+
+    this.html.s2StartButton.onclick = this.s2StartButton_onClick.bind(this)
+  }
+
+  s2StartButton_onClick (e) {
+    const s2Object = this.html.s2Object
+    const startVal = this.html.s2ColourA.value
+    const endVal = this.html.s2ColourB.value
+
+    const s2AnimationSteps = [
+      { background: startVal, offset: 0.0 },
+      { background: endVal, offset: 1.0 },
+    ]
+
+    const s2Timing = {
+      duration: 3000,
+      iterations: Infinity,
+    }
+
+    s2Object.animate(s2AnimationSteps, s2Timing)
   }
 }
 //==============================================================================
